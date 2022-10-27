9eacfb8c8c863e23f0c6f4fe240b77e1e0ec4cdb
Kaue Pereira <kauefontes@outlook.com>
2022-10-26 21:43:49 -0400

    Created object with form inputs items, printing it

--- src/screens/Form/index.tsx
+++ src/screens/Form/index.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
-import Toast from 'react-native-toast-message';
-import uuid from 'react-native-uuid';
+import React, { useState } from 'react'
+import { KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native'
+import uuid from 'react-native-uuid'
 
-import { styles } from './styles';
+import { styles } from './styles'
 
-import { Input } from '../../components/Input';
-import { Button } from '../../components/Button';
-import { HeaderForm } from '../../components/HeaderForm';
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+import { HeaderForm } from '../../components/HeaderForm'
 
 export function Form() {
+  const [name, setName] = useState("")
+  const [user, setUser] = useState("")
+  const [password, setPassword] = useState("")
 
+  function handleSubmit(){
+    const id = uuid.v4()
+    const data = {
+      id,
+      name,
+      user,
+      password
+    }
+
+    console.log(data)
+
+  }
+  
 
   return (
     <KeyboardAvoidingView
@@ -25,20 +40,24 @@ export function Form() {
           <View style={styles.form}>
             <Input
               label="Nome do serviço"
+              onChangeText={setName}
             />
             <Input
               label="E-mail ou usuário"
               autoCapitalize="none"
+              onChangeText={setUser}
             />
             <Input
               label="Senha"
               secureTextEntry
+              onChangeText={setPassword}
             />
           </View>
 
           <View style={styles.footer}>
             <Button
               title="Salvar"
+              onPress={handleSubmit}
             />
           </View>
         </ScrollView>
