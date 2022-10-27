95ec88ef3d8f861160df916b9666f02424a6c399
Kaue Pereira <kauefontes@outlook.com>
2022-10-26 23:58:18 -0400

    listing and deleting items from  persisted item

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,37 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { FlatList, Text, View } from 'react-native';
 
 import { Card, CardProps } from '../../components/Card';
 import { HeaderHome } from '../../components/HeaderHome';
 import { useFocusEffect } from '@react-navigation/native';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage'
+import Toast from 'react-native-toast-message';
 
 import { styles } from './styles';
 import { Button } from '../../components/Button';
 
 export function Home() {
   const [data, setData] = useState<CardProps[]>([]);
+  const { getItem, setItem } = useAsyncStorage("@keepass:formData")
+
+  async function handleFetchData(){
+    const response = await getItem()
+    const data = response ? JSON.parse(response) : []
+    setData(data)
+    console.log(data)
+  }
+
+  async function handleRemove(id: string) {
+    const response = await getItem()
+    const previousData = response ? JSON.parse(response) : []
+    const newData = previousData.filter((item: CardProps) => item.id !== id)
+    setItem(JSON.stringify(newData))
+    handleFetchData()
+  }
+
+  useFocusEffect(useCallback(() => {
+    handleFetchData()
+  },[]))
 
   return (
     <View style={styles.container}>
